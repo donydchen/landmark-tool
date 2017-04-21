15d01d6ae23fdd8c3d4e786a1f2bd461a8920e75
chenyvehtung <chenyvehtung@163.com>
2017-04-21 17:36:27 +0800

    add landmark event

--- src/main_win.py
+++ src/main_win.py
@@ -3,6 +3,7 @@ from PyQt4 import QtCore, QtGui
 from ui.main_win_ui import Ui_Landmark
 from utils import get_imgnames
 from os.path import expanduser, join
+import json
 
 
 __all__ = ["MainWin"]
@@ -16,6 +17,10 @@ class MainWin(QtGui.QMainWindow):
 
         self.init_toolbar()
         self.setup_connect()
+        self.point_dict = {}
+        self.point_rad = 3
+        self.point_pen = QtGui.QPen(QtCore.Qt.red)
+        self.point_pen.setWidth(2)
 
     def setup_connect(self):
         self.btnOpen.triggered.connect(self.file_dialog)
@@ -45,15 +50,38 @@ class MainWin(QtGui.QMainWindow):
         self.imgnames = get_imgnames(self.pathname)
         model = QtGui.QStandardItemModel()
         for imgname in self.imgnames:
+            self.point_dict[unicode(imgname)] = []
             item = QtGui.QStandardItem(unicode(imgname))
             model.appendRow(item)
         self.ui.nameList.setModel(model)
 
     def show_img(self, index):
-        cur_img = unicode(join(str(self.pathname), str(index.data().toString())))
-        scene = QtGui.QGraphicsScene()
-        pixmap = QtGui.QPixmap(cur_img)
-        img_width = self.ui.imgShow.size().width()
-        pixmap = pixmap.scaledToWidth(int(img_width) - 5)
-        scene.addPixmap(pixmap)
-        self.ui.imgShow.setScene(scene)
+        self.scene = QtGui.QGraphicsScene()
+        self.ui.imgShow.setScene(self.scene)
+        self.cur_img = unicode(str(index.data().toString()))
+        pixmap = QtGui.QPixmap(join(str(self.pathname), self.cur_img))
+        img_width = self.ui.imgShow.size().width() - 5
+        self.dis_scale = float(pixmap.width()) / float(img_width)
+        pixmap = pixmap.scaledToWidth(img_width)
+        pixmap = QtGui.QGraphicsPixmapItem(pixmap, None, self.scene)
+        # append existing point
+        for item in self.point_dict[self.cur_img]:
+            self.scene.addEllipse(item[0] - self.point_rad, item[1] - self.point_rad,
+                                  self.point_rad * 2, self.point_rad * 2,
+                                  self.point_pen)
+
+        pixmap.mousePressEvent = self.point_select
+
+    def point_select(self, event):
+        point = event.pos()
+        self.point_dict[self.cur_img].append([point.x(), point.y()])
+        self.scene.addEllipse(point.x() - self.point_rad, point.y() - self.point_rad,
+                              self.point_rad * 2, self.point_rad * 2,
+                              self.point_pen)
+
+    def save_points(self):
+        with open("landmarks.json", "wb") as f:
+            json.dump(self.point_dict, f)
+
+    def closeEvent(self, event):
+        self.save_points()
